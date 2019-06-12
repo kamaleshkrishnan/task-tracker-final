18690cc7f9cfc50519edf0ab5d179b58e70703d9
Krishnan <C050740@cvscaremark.com>
2019-06-11 23:31:02 -0400

    commit for changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+
+import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild, TemplateRef, ViewContainerRef, Input, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  taskForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) {}
+  
+  ngOnInit() {
+    this.createTaskForm();
+  }
+  
+  createTaskForm(){
+    this.taskForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname:  ['', Validators.required],
+      assigned:  ['', Validators.required]
+    })
+  }
+  
 }
